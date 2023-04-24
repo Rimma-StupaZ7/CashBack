47002681d8de8c72ef6c70e0b07399ef216ce28a
rimma test <valentin.rimma@gmail.com>
2023-04-24 15:23:39 +0400

    fix problem

---
+++ src/test/java/ru/netology/CashBack/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.CashbackHackService;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        cashbackHackService
+    }
+}
