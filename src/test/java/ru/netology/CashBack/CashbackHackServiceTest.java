6c74459888dbee796f1f3fd6f0a186f6ea8a65bf
rimma test <valentin.rimma@gmail.com>
2023-04-27 15:29:55 +0400

    Junit4 test

--- src/test/java/ru/netology/CashBack/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashBack/CashbackHackServiceTest.java
@@ -1,12 +1,47 @@
-package ru.netology.CashbackHackService;
+package ru.netology.CashBack;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+    @Test
+    public void shouldSeeAdditionalAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+        int expected = 300;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
 
-    @org.testng.annotations.Test
-    public void testRemain() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        cashbackHackService
+    @Test
+    public void AmountWhenSpentAThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void WenSpendMin() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShowAmountWhenFullThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 0;
+        assertEquals(expected, actual);;
+    }
+
+    @Test
+    public void LittleOverAThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1030);
+        int expected = 970;
+        assertEquals(expected, actual);;
     }
 }
